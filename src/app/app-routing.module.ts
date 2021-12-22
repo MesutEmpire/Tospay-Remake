d7111374cccc2f19bce7a7657f22c9dc39e4a615
Samuel Wainaina <samwainaina01@gmail.com>
2021-12-22 21:07:17 +0300

    Add Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomepageComponent } from './homepage/homepage.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PartnersComponent } from './partners/partners.component';
+import { WhatwedoComponent } from './whatwedo/whatwedo.component';
+import { ProductsComponent } from './products/products.component';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { ClientsComponent } from './clients/clients.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { EnquiryComponent } from './enquiry/enquiry.component';
+import { FooterComponent } from './footer/footer.component';
+import { StoryComponent } from './story/story.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    LandingPageComponent,
+    PartnersComponent,
+    WhatwedoComponent,
+    ProductsComponent,
+    BenefitsComponent,
+    ClientsComponent,
+    CertificationsComponent,
+    EnquiryComponent,
+    FooterComponent,
+    StoryComponent
   ],
   imports: [
     BrowserModule,
