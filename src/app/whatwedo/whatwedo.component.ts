5c3b962d7bb530caa7ded9654cb6bb8e6a0f15c4
MesutEmpire <MesutEmpire@users.noreply.github.com>
2023-01-11 23:55:26 +0000

    Deploying to gh-pages from  @ 2bc2264a8b4016452187003e7a08cd2d443976c1 🚀

---
+++ src/app/whatwedo/whatwedo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-whatwedo',
+  templateUrl: './whatwedo.component.html',
+  styleUrls: ['./whatwedo.component.scss']
+})
+export class WhatwedoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
